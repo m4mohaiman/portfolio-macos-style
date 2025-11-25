c4338d89ca16acdb6cb4b56fa174f4594ba7fe6d
Mohaiman <m4mohaiman@gmail.com>
2025-11-25 12:19:17 +0600

    zustan add and globle state for open add

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,9 +3,11 @@ import {dockApps} from "#constants/index.js";
 import {Tooltip} from 'react-tooltip'
 import {useGSAP} from "@gsap/react";
 import gsap from 'gsap';
+import useWindowStore from "#store/window.js";
 
 
 const Dock = () => {
+    const { openWindow , closeWindow , windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -47,6 +49,18 @@ const Dock = () => {
 
     const toggleApp = (app) => {
         // TODO Implement open window Logic
+        if(!app.canOpen) return;
+        
+        const window = windows[app.id];
+        
+        if(window.isOpen){
+            closeWindow(app.id);
+        }else{
+            openWindow(app.id);
+        }
+
+        console.log(windows)
+
     }
     return (
         <section id="dock">
